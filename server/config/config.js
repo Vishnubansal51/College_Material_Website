623004362ccd4f60e8b58260c21171392833f8a8
Vishnubansal51 <bansalvishnu51@gmail.com>
2024-07-23 14:09:52 +0530

    updated index.js for database and added config.js

---
+++ server/config/config.js
@@ -0,0 +1,42 @@
+const { URL } = require('url');
+
+const parseDatabaseUrl = (url) => {
+  const parsedUrl = new URL(url);
+  return {
+    username: parsedUrl.username,
+    password: parsedUrl.password,
+    database: parsedUrl.pathname.split('/')[1],
+    host: parsedUrl.hostname,
+    port: parsedUrl.port,
+    dialect: 'mysql',
+    timezone: '+05:30'
+  };
+};
+
+module.exports = {
+  development: process.env.DATABASE_URL ? parseDatabaseUrl(process.env.DATABASE_URL) : {
+    username: 'root',
+    password: null,
+    database: 'database_development',
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql',
+    timezone: '+05:30'
+  },
+  test: process.env.DATABASE_URL ? parseDatabaseUrl(process.env.DATABASE_URL) : {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql'
+  },
+  production: process.env.DATABASE_URL ? parseDatabaseUrl(process.env.DATABASE_URL) : {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql'
+  }
+};
--- server/models/index.js
+++ server/models/index.js
@@ -1,25 +1,18 @@
+'use strict';
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: config.dialect,
-    timezone: config.timezone,
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    port: config.port,
-    dialect: config.dialect,
-    timezone: config.timezone,
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
