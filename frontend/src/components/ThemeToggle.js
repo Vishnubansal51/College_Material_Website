7fc39c5366f9668a4ae038f97701152e870e6eda
Vishnubansal51 <bansalvishnu51@gmail.com>
2024-07-24 21:27:15 +0530

    add src folder

---
+++ frontend/src/components/ThemeToggle.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const ThemeToggle = ({ toggleTheme }) => {
+//   return (
+//     <button onClick={toggleTheme} className="btn btn-secondary">
+//       Toggle Theme
+//     </button>
+//   );
+// };
+
+// export default ThemeToggle;
